1d8475bbade8baaa89caa7743440febd2bc2eecb
Rigoberto Rascon <rigobertorascon5@gmail.com>
2022-06-14 09:18:28 -0700

    :

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+import { Main } from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Main />
     </div>
   );
 }
---
+++ client/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <section className="flex flex-1 flex-col p-5 bg-slate-100 min-h-screen min-w-screen">
+      <div className="md:w-3/5 md:max-w-lg self-center">
+        <div className="flex justify-center"></div>
+        <div className="flex p-1 justify-around items-center my-4 bg-white border-2 shadow-sm rounded-full">
+          <input
+            type="text"
+            className="w-full ml-2 md:ml-4 text-gray-800 focus:outline-none focus:shadow-outline"
+          />
+          <button class="bg-cyan-500 hover:bg-cyan-600 text-white w-24 p-1 rounded-full text-sm font-bold">
+            Try !
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
