7d46a4a917ed3ed9d2364b1fc1c0549a617b22b7
Rigoberto Rascon <rigobertorascon5@gmail.com>
2022-06-14 17:52:44 -0700

    update / switched to commonjs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from "express";
-import fetch from "node-fetch";
+// import express from "express";
+// import fetch from "node-fetch";
+const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 
