42e31c501a99087ee84fbd2178272cb96d6dd61e
Rigoberto Rascon <rigobertorascon5@gmail.com>
2022-06-14 18:13:09 -0700

    fix / file extension

--- index.mjs
+++ index.mjs
@@ -1,7 +1,5 @@
-// import express from "express";
-// import fetch from "node-fetch";
-const express = require("express");
-const fetch = require("node-fetch");
+import express from "express";
+import fetch from "node-fetch";
 
 const app = express();
 
